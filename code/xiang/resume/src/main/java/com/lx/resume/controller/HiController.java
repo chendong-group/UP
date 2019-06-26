574561c8ef4c92bb178d7ba8d4c0b9a270885b7c
霸气小香蕉 <a13018015251@163.com>
2019-06-26 16:26:29 +0800

    学习搭建springboot的项目文件

---
+++ code/xiang/resume/src/main/java/com/lx/resume/controller/HiController.java
@@ -0,0 +1,14 @@
+package com.lx.resume.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HiController {
+
+    @RequestMapping("/hi")
+    public String hi()
+    {
+        return "index";
+    }
+}
